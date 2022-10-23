e17eac96259e236df62a40e692d5658f0c5fb09b
jimmy omar <jimmy@gmail.com>
2022-10-23 17:02:07 -0500

    Agregar archivos de proyecto.

---
+++ Tarea1_Jimmy Toapanta/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tarea1_Jimmy_Toapanta
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Login());//habilitar la nacegacion entre ventanas 
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Tarea1_Jimmy Toapanta/Login.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tarea1_Jimmy_Toapanta
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+    
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = "jotoapanta";
+            string contraseña = "12345";
+
+            string tUsuario = txtUsuario.Text;
+            string tContraseña = txtContraseña.Text;
+
+            if (usuario == tUsuario & contraseña == tContraseña) {
+
+                DisplayAlert("Registro de Notas", "Bienvenido :)", "Cerrar");
+
+                Navigation.PushAsync(new Registro());  //instancia entre ventanas de navegacion
+            }
+            else
+
+            {
+
+                DisplayAlert("Alerta", "Usuario No existe!!", "Cerrar");
+
+            }
+
+           
+        
+
+        }
+       
+
+
+    }
+}
